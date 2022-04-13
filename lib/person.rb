70208fb317b07e1f0abf3f896ce03a89758dedfa
Thomas Dimperio <twdimperio@gmail.com>
2022-04-13 11:33:21 -0600

    commit tree edcb3cbbecad40003315fff919ec78cd16ba0815

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
+require "pry"
+
 class Person
-  # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+bob_attributes = {name: "Bob", hair_color: "Brown"}
+susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
+
+binding.pry
